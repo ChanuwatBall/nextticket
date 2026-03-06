d94c81e89a4e1e58dcddc93aee0fb07a6c560d8f
Ball <chanuwat.1005@hotmail.com>
2026-03-06 21:31:47 +0700

    update layout

--- src/components/BookingLayout.tsx
+++ src/components/BookingLayout.tsx
@@ -15,7 +15,7 @@ const BookingLayout = ({ children, currentStep = 1, showSteps = true, title }: B
   return (
     <div className="min-h-screen bg-background flex flex-col">
      
-      <header className=" text-primary-foreground px-4 py-3  gap-3 shadow-md sticky top-0 z-50">
+      <header className=" text-primary-foreground px-4 py-3  gap-3   sticky top-0 z-50">
         {/* <Link to="/" className="flex items-center gap-2">
           <Bus className="h-6 w-6" />
           <h1 className="text-lg font-bold tracking-tight">Nex Express</h1>
@@ -27,21 +27,22 @@ const BookingLayout = ({ children, currentStep = 1, showSteps = true, title }: B
           </button>
           <h2 className="text-lg font-bold">{title}</h2>
         </div>)}
-        {showSteps && currentStep && (
-        <div className=" border-b border-none " >
+        
+      </header>
+
+      
+      {/* Content */}
+      <main className="flex-1 max-w-lg mx-auto w-full pb-8 pt-2 bg-white" style={{zIndex:999}} >
+       {/* Step Indicator */}
+      {showSteps && currentStep && (
+        <div className=" border-b border-none pb-8" >
           <div className="max-w-lg mx-auto">
             <StepIndicator currentStep={currentStep} />
           </div>
         </div>
       )}
-      </header>
 
-      {/* Step Indicator */}
-      
 
-      {/* Content */}
-      <main className="flex-1 max-w-lg mx-auto w-full pb-8 pt-4">
-       
         {children}
       </main>
     </div>
